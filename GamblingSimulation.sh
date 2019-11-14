c445982132c7a06a403aa7341270f5ee54cadf2e
Narayan  Mahadevan <narayan@Narayans-MacBook-Pro.local>
2019-11-14 20:28:15 +0530

    Add: dailyBetting to exit if either won or lost

--- GamblingSimulation.sh
+++ GamblingSimulation.sh
@@ -7,9 +7,12 @@ WON=1
 LOSS=0
 STAKE=100
 BET_AMOUNT=1
+DAILY_RESIGN_PERC=50
 
 #Variable
 dailyBetAmount=$STAKE
+maxTotalDailyAmount=$(($STAKE+$STAKE*DAILY_RESIGN_PERC/100))
+minTotalDailyAmount=$(($STAKE-$STAKE*DAILY_RESIGN_PERC/100))
 
 function makeBet() {
 	local betStatus=$((RANDOM%2))
@@ -26,5 +29,13 @@ function makeBetAndUpdate() {
    dailyBetAmount=$(($dailyBetAmount+$betAmount))
 }
 
-makeBetAndUpdate
+function dailyBetting() {
+	while [[ $dailyBetAmount -lt $maxTotalDailyAmount && 
+            $dailyBetAmount -gt $minTotalDailyAmount ]]
+	do
+		makeBetAndUpdate
+	done
+}
 
+dailyBetting
+echo $dailyBetAmount
