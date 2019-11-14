54fe71eb36e58499db1874b214f47ad53583ca3f
Narayan  Mahadevan <narayan@Narayans-MacBook-Pro.local>
2019-11-14 14:18:29 +0530

    Add: makeBet to win or loose Bet Amount

--- GamblingSimulation.sh
+++ GamblingSimulation.sh
@@ -1,6 +1,19 @@
 #!/bin/bash -x
 
 echo "Welcome to Gambling Simulator Program"
-
+WON=1
+LOSS=0
 STAKE=100
-BET=1
+BET_AMOUNT=1
+
+function makeBet() {
+	local betStatus=$((RANDOM%2))
+	if [ $WON -eq $betStatus ]
+	then 
+   	echo $BET_AMOUNT
+	else 
+		echo -$BET_AMOUNT
+	fi
+}
+
+makeBet
